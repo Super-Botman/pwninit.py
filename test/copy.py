3c009bc2a49bae1f54c8d4d16f60311a1376feae
installer <installer@gmail.com>
2024-11-26 23:07:26 +0100

    apply gitignore

--- test/copy.py
+++
@@ -1,6 +0,0 @@
-import os
-
-file = open("files.txt", "r")
-
-for line in file.readlines():
-    os.system('cp "%s" ./bins' % line[:-1])
--- test/init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find ~/Documents/challenges -type f -executable -exec file -i '{}' \; | grep "application/.*; charset=binary" | cut -f1 -d":" > files.txt
